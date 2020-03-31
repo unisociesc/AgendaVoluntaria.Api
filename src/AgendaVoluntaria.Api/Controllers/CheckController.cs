5a95f9ce3b9d1b83423dd9912113890fd6236785
Gabriel Meyer <ghmeyer0@gmail.com>
2020-03-30 22:12:06 -0300

    Adicionado rota de check in e check out

---
+++ src/AgendaVoluntaria.Api/Controllers/CheckController.cs
@@ -0,0 +1,31 @@
+﻿using AgendaVoluntaria.Api.Controllers.Core;
+using AgendaVoluntaria.Api.Utils.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AgendaVoluntaria.Api.Controllers
+{
+    public class CheckController : CoreController
+    {
+        public CheckController(INotifier notifier) : base(notifier)
+        {
+        }
+
+        [Route("in")]
+        [HttpPost]
+        public async Task<IActionResult> In ()
+        {
+            return Ok();
+        }
+
+        
+        [HttpPost]
+        [Route("out")]
+        public async Task<IActionResult> Out()
+        {
+            return Ok();
+        }
+
+
+    }
+}
