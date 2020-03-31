9bad58b29b573a9f8e491b762490369678aba80f
Gabriel Meyer <ghmeyer0@gmail.com>
2020-03-31 01:38:49 -0300

    Atualizado migration Manual

--- src/AgendaVoluntaria.Api/Models/Context.cs
+++ src/AgendaVoluntaria.Api/Models/Context.cs
@@ -25,28 +25,17 @@ namespace AgendaVoluntaria.Api.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasData(
-                new User { Id = Guid.Parse("bfbd39c6-76cb-4f49-8351-09ac4b64cb9c"), Email = "ghmeyer0@gmail.com", Name = "Gabriel Helko Meyer", Password = "4edc2113d0937fcc5f79c2f3af0a6aa30fa8fb545bfed7d06693d2c909399600", Role = "admin", CreateAt = new DateTime(2020, 3, 29), UpdateAt = new DateTime(2020, 3, 29) }
+                new User { Id = Guid.Parse("bfbd39c6-76cb-4f49-8351-09ac4b64cb9c"), Email = "ghmeyer0@gmail.com", Name = "Gabriel Helko Meyer", Password = "4edc2113d0937fcc5f79c2f3af0a6aa30fa8fb545bfed7d06693d2c909399600", Role = "admin", CreateAt = new DateTime(2020, 3, 29), UpdateAt = new DateTime(2020, 3, 29) },
+                new User { Id = Guid.Parse("95cf0000-dbca-d9ec-3724-08d7d51e7020"), Email = "ricardo.pfitscher@unisociesc.com.br",Name = "Ricardo Pfitscher", Password = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", Role = "admin", CreateAt = new DateTime(2020, 3, 29), UpdateAt = new DateTime(2020, 3, 29) },
+                new User { Id = Guid.Parse("1e9e0000-6d1c-bc1b-eb06-08d7d524408d"), Email = "eslin@eslin.com", Name = "Éslin Makicine Martins", Password = "abad878fb17fb8710d8ac8c4612f379ee0ff9a0ad530bbe9729b7256f178f7bd", Role = "admin", CreateAt = new DateTime(2020, 3, 29), UpdateAt = new DateTime(2020, 3, 29) },
+                new User { Id = Guid.Parse("3a480000-248f-9e93-51c3-08d7d5261675"), Email = "johnnyrtrentin@gmail.com", Name = "johnny", Password = "98bb0701a145990b4f0e0880398010d97c3d24eb189e8c80f818f6bc062a86d0", Role = "admin", CreateAt = new DateTime(2020, 3, 29), UpdateAt = new DateTime(2020, 3, 29) }
             );
 
-            modelBuilder.Entity<Shift>(entity =>
+            modelBuilder.Entity<Volunteer>(entity =>
             {
-                var lastShift = new DateTime(2020, 03, 20, 0, 0, 0, 0, DateTimeKind.Local);
-                List<Shift> list = new List<Shift>();
-                for (int i = 0; i < 1105; i++)
-                {
-                    list.Add(new Shift
-                    {
-                        Id = NewId.NextGuid(),
-                        Begin = lastShift,
-                        End = lastShift.AddHours(6),
-                        MaxVolunteer = lastShift.Hour == 0 && lastShift.AddHours(6).Hour == 6 ? 4 : 20,
-                        CreateAt = new DateTime(2020, 03, 30, 23, 0, 0, DateTimeKind.Local),
-                        UpdateAt = new DateTime(2020, 03, 30, 23, 0, 0, 0, DateTimeKind.Local)
-                    });
-                    lastShift = lastShift.AddHours(6);
-                }
-
-                entity.HasData(list);
+                entity.HasData(
+                    new Volunteer { Id = Guid.Parse("60180000-5e87-f520-b74d-08d7d52122bd"), Ra = 121815278, Course = "Engenharia", NeedPsico = false, IdUser = Guid.Parse("95cf0000-dbca-d9ec-3724-08d7d51e7020") }
+                    );
             });
         }
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
