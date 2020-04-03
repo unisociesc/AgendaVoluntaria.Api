a3c3c042650df87ae10a8391c54f92420d134bb8
Allan <allanrodrigol@gmail.com>
2020-04-02 21:50:00 -0300

    Adição service para envio de e-mail (issue #9)

---
+++ src/AgendaVoluntaria.Api/Controllers/EmailController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using AgendaVoluntaria.Api.Controllers.Core;
+using AgendaVoluntaria.Api.Models;
+using AgendaVoluntaria.Api.Services.Interfaces;
+using AgendaVoluntaria.Api.Utils.Interfaces;
+using AgendaVoluntaria.Api.Views;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaVoluntaria.Api.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class EmailController : CoreController
+    {
+        private readonly INotifier _notifier;
+        private readonly IEmailService _emailService;
+        
+        public EmailController(INotifier notifier, IEmailService emailService) : base(notifier)
+        {
+            _notifier = notifier;
+            _emailService = emailService;
+        }
+
+        [HttpGet]
+        public virtual IActionResult SendAsync()
+        {
+            _emailService.SendAsync("allanrodrigol@gmail.com","Teste","Olá mundo!");
+            return CustomResponse("Email enviado!");
+        }
+
+    }
+}
---
+++ src/AgendaVoluntaria.Api/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Threading.Tasks;
+
+namespace AgendaVoluntaria.Api.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        public Task SendAsync(string to, string subject, string message);
+    }
+}
