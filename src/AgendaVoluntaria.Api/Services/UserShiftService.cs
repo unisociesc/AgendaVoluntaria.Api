d44cb25ade465540edea51a29c0e2289cddc885d
Gabriel Meyer <ghmeyer0@gmail.com>
2020-04-03 15:43:57 -0300

    Adicionado metodo para enviar horarios agendados

--- src/AgendaVoluntaria.Api/Services/UserShiftService.cs
+++ src/AgendaVoluntaria.Api/Services/UserShiftService.cs
@@ -5,6 +5,7 @@ using AgendaVoluntaria.Api.Services.Interfaces;
 using AgendaVoluntaria.Api.Utils.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,16 +17,20 @@ namespace AgendaVoluntaria.Api.Services
         private readonly IShiftService _shiftService;
         private readonly IVolunteerService _volunteerService;
         private readonly IUserService _userService;
+        private readonly IEmailService emailService;
 
         public UserShiftService(INotifier notifier,
                                 IShiftService shiftService,
                                 IVolunteerService volunteerService,
                                 IUserService userService,
-                                IUserShiftRepository repository) : base(notifier, repository)
+                                IUserShiftRepository repository,
+                                IEmailService emailService
+            ) : base(notifier, repository)
         {
             _shiftService = shiftService;
             _volunteerService = volunteerService;
             _userService = userService;
+            this.emailService = emailService;
         }
 
         public override async Task<int> CreateAsync(UserShift volunteerShift)
@@ -59,6 +64,50 @@ namespace AgendaVoluntaria.Api.Services
             }
 
             return await base.CreateAsync(volunteerShift);
+        }
+
+        public async Task SendScheduleOfDay()
+        {
+
+            var schedules = await _repository.GetSchedulesOfDay(DateTime.Today.AddDays(1));
+            var shifts = schedules.GroupBy(x => new { x.Begin, x.End }).OrderBy(x => x.Key.Begin).ToList();
+
+            string rowColor = string.Empty;
+            string htmlTable = string.Empty;           
+            foreach (var shift in shifts)
+            {
+                rowColor = rowColor.Equals("#F8F8F8") ? "#f1f1f1" : "#F8F8F8";
+
+                htmlTable += $"<tr style='border-collapse:collapse;box-sizing:border-box;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;color: #333333; background: {rowColor};'>";
+                htmlTable += $"<td style='padding:8px;Margin:0;box-sizing:border-box;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;text-align:center;border-right:1px solid #F8F8F8;font-size:12px;'>{shift.Key.Begin:HH:mm} as {shift.Key.End:HH:mm}</td>";
+                htmlTable += "<td style='padding:8px;Margin:0;box-sizing:border-box;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;text-align:center;border-right:1px solid #F8F8F8;font-size:12px;'>";
+                foreach (var volunteer in schedules)
+                {
+                    if (volunteer.Begin == shift.Key.Begin)
+                    {
+                        htmlTable += $"{volunteer.User.Name} - {volunteer.User.Phone} <br style='box-sizing:border-box;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;'>";
+                    }
+                }
+                htmlTable += "</td>";
+                htmlTable += "</tr>";
+            }
+            
+            string html = string.Empty;
+
+            string path = Directory.GetCurrentDirectory();
+            path = Path.GetFullPath("schedules.html");
+            using (StreamReader reader = new StreamReader(path))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            html = html.Replace("#DAY#", DateTime.Today.AddDays(1).Day.ToString());
+            html = html.Replace("#MONTH#", DateTime.Today.AddDays(1).Month.ToString());
+            html = html.Replace("#TABLE-BODY#", htmlTable);
+            emailService.SendAsync("ghmeyer0@gmail.com", "Triagem COVID-19 - Escala", html);
+            
+            return;
+            
         }
 
         public async Task<List<UserShift>> GetUserShiftsByUser(Guid idUser)
